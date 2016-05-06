fdbe086c33ea6e156eba2b135725cd99671705a6
Nick Verse <nikoskalomiris@gmail.com>
2016-05-06 15:30:32 +0300

    Initial commit

---
+++ BoardConfig.mk
@@ -0,0 +1,38 @@
+# Copyright (C) 2015 The CyanogenMod Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+#
+# This file sets variables that control the way modules are built
+# thorughout the system. It should not be used to conditionally
+# disable makefiles (the proper mechanism to control what gets
+# included in a build is to use PRODUCT_PACKAGES in a product
+# definition file).
+#
+
+# Inherit from common j5
+-include device/samsung/j5-common/BoardConfigCommon.mk
+
+# Inherit from the proprietary version
+-include vendor/samsung/j5ltexx/BoardConfigVendor.mk
+
+# Assert
+TARGET_OTA_ASSERT_DEVICE := j5lte
+
+# Kernel
+TARGET_KERNEL_CONFIG := cyanogenmod_j5nlte_defconfig
+
+# Vendor Init
+TARGET_UNIFIED_DEVICE := true
+TARGET_INIT_VENDOR_LIB := libinit_msm
+TARGET_LIBINIT_DEFINES_FILE := device/samsung/j5lteub/init/init_j5lteub.c

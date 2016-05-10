027a091f36b7445ce9c33f5913827e7d810daf0a
Nick Verse <nikoskalomiris@gmail.com>
2016-05-10 09:59:43 +0300

    Update BoardConfig.mk

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -42,6 +42,7 @@ TARGET_OTA_ASSERT_DEVICE := j5lte
 
 # Kernel
 TARGET_KERNEL_CONFIG := cyanogenmod_j5nlte_defconfig
+TARGET_KERNEL_SOURCE := kernel/samsung/j5lte
 
 # Vendor Init
 TARGET_UNIFIED_DEVICE := true
